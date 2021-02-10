f4104912bf633c3ab5f4e88677b963c6f47973e4
netadmin-str <danil@mirsmpc.ru>
2021-02-10 15:52:44 +0000

    Код для отработки разбора.

---
+++ Show_version_os.py
@@ -0,0 +1,89 @@
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from netmiko import ConnectHandler
+import yaml
+from ntc_templates.parse import parse_output
+
+hosts = ['192.168.1.101']
+
+for host_ip in hosts:
+    sshCli = ConnectHandler(
+        device_type='cisco_ios',
+        host=host_ip,
+        port=22,
+        username='admin',
+        password='cisco'
+    )
+
+    output = sshCli.send_command("sh ver")
+    output_parsed = parse_output(platform="cisco_ios", command="show version", data=output)
+
+    for element in output_parsed:
+        device_id = element['neighbor'].split('.')
+        description = 'Connect to ' + device_id[0]
+        print(host_ip + ' ' + element['local_interface'] + ' - ' + description)
+        commands = ['int ' + element['local_interface'],
+                    'description ' + description]
+        sshCli.send_config_set(commands)
+
+    print(host_ip + ': OK')
+
+'''
+def send_command_to_devices(devices, commands, max_threads=2):
+    data = {}
+    with ThreadPoolExecutor(max_workers=max_threads) as executor:
+        future_ssh = [
+            executor.submit(send_show, device, commands) for device in devices
+        ]
+        for f in as_completed(future_ssh):
+            result = f.result()
+            data.update(result)
+    return data
+'''
+
+''' def send_show(device_dict, commands):
+    if type(commands) == str:
+        commands = [commands]
+    ip = device_dict["ip"]
+    result = {}
+    with ConnectHandler(**device_dict) as ssh:
+        ssh.enable()
+        for command in commands:
+            output = ssh.send_command(command)
+            output_parsed = parse_output(platform="cisco_ios", command="show version", data=output)
+
+            for element in output_parsed:
+                device_id = element['neighbor'].split('.')
+                description = 'Connect to ' + device_id[0]
+                result.update({element['local_interface']: description})
+                commands = ['int ' + element['local_interface'],
+                            'description ' + description]
+                ssh.send_config_set(commands)
+
+    return {ip: result}
+'''
+
+'''if __name__ == "__main__":
+    filename = "devices_all.yaml"
+
+    with open(filename) as f:
+        devices = yaml.safe_load(f)
+    print("Количество устройств:", len(devices))
+
+    all_done = send_command_to_devices(
+        devices, commands="show version", max_threads=16
+    )
+
+    # print(all_done)
+    file = open('description_from_cdp_result.csv', 'w')
+
+    for key, value in all_done.items():
+        for key_int, value_desc in value.items():
+            line = str(key) + ';' + key_int + ';' + value_desc
+            file.write(line + '\n')
+
+    file.close()
+'''
+
+
+
+
